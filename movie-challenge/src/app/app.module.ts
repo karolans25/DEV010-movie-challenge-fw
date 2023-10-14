cb8c0ba562acb80473e4813e1db568bef6bb5cd7
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-10-14 02:30:59 -0500

    create components navbar and footer without style and data
    create NavbarComponent and FooterComponent

--- movie-challenge/src/app/app.module.ts
+++ movie-challenge/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MoviesContainerComponent } from './components/movies-container/movies-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    NavbarComponent,
+    FooterComponent,
+    MoviesContainerComponent
   ],
   imports: [
     BrowserModule,
