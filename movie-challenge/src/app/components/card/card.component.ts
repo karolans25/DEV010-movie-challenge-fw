b5693f3ce71fae7f27a7b50094ea34417e01953c
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-10-17 01:38:47 -0500

    giving styles to cards with mock data
    I used an interface and mocked data to send info to the cards from movies component (it contains the cards of movies)
    I used the Input decorator to pass data from parent (movies) to child (card).

--- movie-challenge/src/app/components/card/card.component.ts
+++ movie-challenge/src/app/components/card/card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+  // @Input() transform!: string;
+  @Input() background!: string;
+  @Input() title!: string;
 
+  ngOnInit(): void {
+      // console.log(this.background);
+      // console.log(this.title?this.title:'noexiste');
+  }
 }
--- movie-challenge/src/app/components/movies/movies.component.ts
+++ movie-challenge/src/app/components/movies/movies.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../interfaces/movie';
+import { MOVIES } from './mock-movies';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit{
+  selection!: number;
+  movies!: Movie[];
+  // caption!: EventTarget | null;
 
+  ngOnInit(): void {
+    this.movies = MOVIES;
+  }
+
+  onCardClicked(index: number): void{
+    this.selection = index;
+    console.log(index);
+  }
 }
