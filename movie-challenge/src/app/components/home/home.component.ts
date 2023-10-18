af7ec3df08d26afab023adbb844dea9bc31f4b22
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-10-18 08:47:15 -0500

    Create options component (search, filter, order)

--- movie-challenge/src/app/components/home/home.component.ts
+++ movie-challenge/src/app/components/home/home.component.ts
@@ -5,22 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit{
-  selectedSearch!: string;
-  selectedFilter!: string;
-  selectedOrder!: string;
+export class HomeComponent{
 
-  filterOptions: string[] = ['Filter by', 'Genres'];
-  orderOptions: string[] = ['Order by', 'Popularity (desc.)', 'Popularity (asc.)', 'Year (desc.)', 'Year (asc.)', 'Vote average (desc.)', 'Vote average (asc.)', 'Vote count (desc.)', 'Vote count (asc.)'];
-
-  ngOnInit(): void{
-    this.selectedFilter = '0';
-    this.selectedOrder = '0';
-  }
-  // capturar() {
-  //   this.selectedFilter = this.selectedFilter;
-  //   this.selectedOrder = this.selectedOrder;
-  // }
   searchByPage(page: number): void{
     console.log('click on page -> ', page+1);
   }
