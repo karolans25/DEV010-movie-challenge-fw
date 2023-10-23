6d8ea7cd9ff7209d7b4e182fdde6274050d14b0d
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-10-23 10:38:18 -0500

    First preview of history user 3

--- movie-time/src/app/components/detail/detail.component.ts
+++ movie-time/src/app/components/detail/detail.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Genre } from 'src/app/interfaces/genre';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit{
+
+  poster!: string;
+  backdrop!: string;
+  title!: string;
+  year!: number;
+  date!: string;
+  vote_average!: number;
+  vote_count!: number;
+  genres!: Genre[];
+  overview!: string;
+
+  constructor(private readonly route: ActivatedRoute){}
+
+  ngOnInit(){
+    this.route.queryParams.subscribe( (params: Params) => {
+      console.log(params);
+
+      this.backdrop = params['backdrop_path']?"url(\'https:\/\/image.tmdb.org/t/p/w780" + params['backdrop_path'] + "\')" : 'url(../../../assets/not-available.png) no-repeat top';
+
+      this.poster = params['poster_path']? "url(\'https:\/\/image.tmdb.org/t/p/w342" + params['poster_path'] + "\')" : 'url(../../../assets/not-available.png)';
+
+      this.title = params['title']? params['title'] : params['name'];
+
+      this.date = params['release_date']? params['release_date'] : params['first_air_date'];
+      this.year = this.getYearOfDate(this.date);
+
+      this.vote_average = params['vote_average'];
+      this.vote_count = params['vote_count'];
+
+      this.genres = params['genres'];
+
+      this.overview = params['overview'];
+    });
+
+  }
+
+  getYearOfDate(date: string): number{
+    const objectDate = new Date(date);
+    return objectDate.getFullYear();
+  }
 
 }
