3f5ddcca301df143dbc92c438cd05365750faf5f
Carolina Pulido Gómez <dev.carolina.pulido01@gmail.com>
2023-10-31 22:53:38 -0500

    Test page-not-found component

--- movie-time/src/app/components/page-not-found/page-not-found.component.spec.ts
+++ movie-time/src/app/components/page-not-found/page-not-found.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
@@ -8,7 +10,7 @@ describe('PageNotFoundComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PageNotFoundComponent]
+      declarations: [PageNotFoundComponent, NavbarComponent, FooterComponent]
     });
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
